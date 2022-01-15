fcb28a346832c80193c7b79fc4e491e417e5ce65
CyntiBinti <cynthia_m@live.com>
2022-01-15 00:34:58 +0000

    display current player fixed

--- connect4_code.js
+++ connect4_code.js
@@ -4,6 +4,8 @@ const displayCurrentPlayer = document.getElementById('current-player');
 const result = document.getElementById('result');
 
 let player1 = "red"
+displayCurrentPlayer.textContent = player1
+displayCurrentPlayer.style.backgroundColor = 'red'
 let win = false
 
 let grid = [
@@ -15,9 +17,9 @@ let grid = [
     [null, null, null, null, null, null, null]
 ]
 
+// takes a turn based on user clicking a slot
 function takeTurn(e) {
-    const id = e.target.id   // 'row1-col1'   ________x
-    // 'rowY-colX' 
+    const id = e.target.id   // 'row1-col1' // 'rowY-colX' 
     console.log(`id is: ${id}`)
 
     const rowNum = id[3]
@@ -32,10 +34,14 @@ function takeTurn(e) {
             grid[lowestAvailableRow][colNum - 1] = "red"
             document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = 'red';
             player1 = "yellow"
+            displayCurrentPlayer.textContent = player1
+            displayCurrentPlayer.style.backgroundColor = 'yellow'
         } else {
             grid[lowestAvailableRow][colNum - 1] = "yellow"
             document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = 'yellow';
             player1 = "red"
+            displayCurrentPlayer.textContent = player1
+            displayCurrentPlayer.style.backgroundColor = 'red'
         }
     }
 
@@ -43,7 +49,7 @@ function takeTurn(e) {
     console.log(grid)
 }
 
-
+// find the lowest available empty slot in a column and row
 function getLowestAvailableRowInColumn(columnNumber, myGrid) {
     for (let i = 5; i >= 0; i--) {
         if (myGrid[i][columnNumber - 1] === null) {
@@ -54,6 +60,7 @@ function getLowestAvailableRowInColumn(columnNumber, myGrid) {
     return null;
 }
 
+// reset's game when reset button clicked
 document.getElementById('reset-button').onclick = () => {
         grid = [
             [null, null, null, null, null, null, null],
@@ -63,8 +70,10 @@ document.getElementById('reset-button').onclick = () => {
             [null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null]
         ]
-        player = 'red'
+        player1 = 'red'
         win = false
+        displayCurrentPlayer.textContent = player1
+        displayCurrentPlayer.style.backgroundColor = 'red'
         
         let oldGrid = document.getElementsByClassName('col')
         for (x of oldGrid) {
