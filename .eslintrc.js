93628f1f30bfc8bf8c614a55e8f884d0259a9412
CyntiBinti <cynthia_m@live.com>
2022-02-01 16:11:23 +0000

    eslint downloaded

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "indent": [
+            "error", 4
+        ]
+    }
+}
